9f6fbcfa86cb30fa90f4a82f5d235bac5f77f501
Caio Carvalho <ccarvalho451@gmail.com>
2020-01-21 18:45:10 -0300

    add location field to devs register

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,30 +1,9 @@
 const { Router } = require("express");
-const axios = require("axios");
 
-const Dev = require("./models/Dev");
+const devController = require("./controllers/DevController");
 
 const routes = Router();
 
-routes.post("/users", async (req, res) => {
-  const { github_username, techs } = req.body;
-
-  const response = await axios.get(
-    `https://api.github.com/users/${github_username}`
-  );
-
-  const { name = login, avatar_url, bio } = response.data;
-
-  const arrayTechs = techs.split(",").map(tech => tech.trim());
-
-  const dev = await Dev.create({
-    github_username,
-    name,
-    avatar_url,
-    bio,
-    techs: arrayTechs
-  });
-
-  return res.json(dev);
-});
+routes.post("/users", devController.store);
 
 module.exports = routes;
