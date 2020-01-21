ca1a9732bf7c0bf115447124ab1b9af80eda3971
Caio Carvalho <ccarvalho451@gmail.com>
2020-01-21 19:33:39 -0300

    techs to lowercase

--- backend/src/utils/StringToArray.js
+++ backend/src/utils/StringToArray.js
@@ -1,3 +1,3 @@
 module.exports = function StringToArray(arrayAsString) {
-  return arrayAsString.split(",").map(tech => tech.trim());
+  return arrayAsString.split(",").map(tech => tech.trim().toLowerCase());
 };
