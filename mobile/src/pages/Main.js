c47ac0d40d1ddc6223bff4d4fe19dfd281fe5f29
caiocp <ccarvalho451@gmail.com>
2020-03-01 14:58:16 -0300

    add application routes

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,6 @@
+import React from "react";
+import MapView from "react-native-maps";
+
+export default function Main() {
+  return <MapView styles={{ flex: 1 }} />;
+}
---
+++ mobile/src/routes.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Main from "./pages/Main";
+import Profile from "./pages/Profile";
+
+export default function Routes() {
+  const Stack = createStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          title: "Dev Finder",
+          headerStyle: { backgroundColor: "#7159c1" },
+          headerTitleAlign: "center",
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold"
+          }
+        }}
+      >
+        <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="Profile" component={Profile} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
